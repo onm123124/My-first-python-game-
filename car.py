e42342e4afb78fb13b9e911c0bac337f716cc89f
onm123123 <112973174+onm123124@users.noreply.github.com>
2023-01-21 08:50:13 -0700

    made the car movable and some other shets

--- car.py
+++ car.py
@@ -2,6 +2,6 @@ import pygame
 class Car():
    def __init__(self):
         self.sprite = pygame.image.load('Sprites/car.png')
-        self.position = pygame.Vector2(750,750)
-        self.velocity = pygame.Vector2(1,0)
-        self.acceleration = 0.1
+        self.position = pygame.Vector2(750,1080)
+        self.velocity = pygame.Vector2(0,-10)
+        self.acceleration = 0.0
--- main.py
+++ main.py
@@ -5,34 +5,44 @@ from Background import background
 from pygame.math import Vector2
 
 def main():
-    screen = pygame.display.set_mode((0,0))
-    car = Car()
-    car_position = car.position
-    car_velocity = car.velocity
-    car_velocity.x += car.acceleration
-    car_velocity.y += car.acceleration
-    car_position.x += car_velocity.x
-    car_position.y += car_velocity.y
-    screen.blit(car.sprite, (car_position.x, car_position.y))
-    #making car
-
     pygame.init()
-   
-    pygame.display.set_caption('Highway maniac')
-    #title and stuff
-    screen.blit(background().sprite,(0,0))
-    #Background
 
+    car = Car()
+    car_velocity = car.velocity
+    screen_width, screen_height = pygame.display.get_surface,pygame.display.get_surface
+    screen = pygame.display.set_mode((1920,1080))
+    car_position = pygame.Vector2(car.position)
+    
+    
+
+    pygame.display.set_caption('Highway maniac')
+    
     pygame.display.update()
-    #The background and stuff
-     
-    running = True #change this to "True" for forever running
+    running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
                 pygame.quit()
-    #infinite running loop if needed
+        screen.blit(background().sprite,(0,0))
+        car_velocity.x += car.acceleration
+        car_velocity.y += car.acceleration
+        car_position += car_velocity
+        screen.blit(car.sprite, (car_position.x, car_position.y))
+        pygame.display.update()
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            car.velocity.x = -10 # <-- move left
+        elif keys[pygame.K_d]:
+            car.velocity.x = 10 # <-- move right
+        else:
+            car.velocity.x = 0 # <-- stop moving        
+                #make car go back to center when it get out
+        if car_position.x > screen.get_width() or car_position.x < 0 or car_position.y > screen.get_height() or car_position.y < 0:
+            car_position = pygame.Vector2(750,1080)
+
+        pygame.display.update()
 
 if __name__ == "__main__":
     main() #end Main sequence
